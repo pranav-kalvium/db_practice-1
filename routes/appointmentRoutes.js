13ce2456f348361dac9c90aedc1fe121245221af
pranav-kalvium <pranavprakash6410@gmail.com>
2025-05-13 02:12:54 +0530

    Update appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -8,24 +8,24 @@ router.post('/appointments', async (req, res) => {
   const { petName, date, vetId, ownerId } = req.body;
 
   if (!petName || !date || !vetId || !ownerId) {
-    return res.status(400).json({ message: "Saari details de bhai!" });
+    return res.status(400).json({ message: " " });
   }
 
   const vet = await User.findById(vetId);
   const owner = await User.findById(ownerId);
 
   if (!vet || vet.role !== 'vet') {
-    return res.status(404).json({ message: "Vet mil nahi raha bhai!" });
+    return res.status(404).json({ message: " " });
   }
 
   if (!owner || owner.role !== 'petOwner') {
-    return res.status(404).json({ message: "Pet Owner gayab hai bhai!" });
+    return res.status(404).json({ message: " " });
   }
 
   const newAppointment = new Appointment({ petName, date, vetId, ownerId });
   await newAppointment.save();
 
-  res.status(201).json({ message: "Appointment fix ho gaya! 🐶📅", appointment: newAppointment });
+  res.status(201).json({ message: "Appointment fixed", appointment: newAppointment });
 });
 
 module.exports = router;
