0425a54e1c0e2774c2bea150f053e25e0db08f90
pranav-kalvium <pranavprakash6410@gmail.com>
2025-05-13 02:12:02 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,22 +7,22 @@ router.post('/register', async (req, res) => {
   const { username, email, role } = req.body;
 
   if (!username || !email || !role) {
-    return res.status(400).json({ message: "Sab fields bhar bhai!" });
+    return res.status(400).json({ message: " " });
   }
 
   if (!['petOwner', 'vet'].includes(role)) {
-    return res.status(400).json({ message: "Role ya toh petOwner hoga ya vet!" });
+    return res.status(400).json({ message: " " });
   }
 
   const emailExists = await User.findOne({ email });
   if (emailExists) {
-    return res.status(400).json({ message: "Ye email already use ho chuka hai!" });
+    return res.status(400).json({ message: " " });
   }
 
   const newUser = new User({ username, email, role });
   await newUser.save();
 
-  res.status(201).json({ message: "User register ho gaya! 🎉", user: newUser });
+  res.status(201).json({ message: "User registered! 🎉", user: newUser });
 });
 
 module.exports = router;
